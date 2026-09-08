f4d472f589a2f9f9768257407899baea9f5bfaec
AutofillMe <113013764+AutofillMe@users.noreply.github.com>
2026-09-08 12:01:24 +0900

    added docstrings

--- checkConfig.py
+++ checkConfig.py
@@ -39,13 +39,36 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def createDictKeys(req: dict, keyLine: list[str]) -> dict[str, list]:
-    for key in keyLine:
+def createDictKeys(req: dict, keys: list[str]) -> dict[str, list]:
+    """
+    Task:
+    Custom logic for creating the empty requirement dict based on the items:
+    Name of Setting (Human readable), name of setting (variable name), expected result, fix
+    ---
+    IN: empty dictionary
+    IN: list containing the item keys
+    OUT: dict with keys
+    """
+    for key in keys:
         req[key] = []
     return req
 
 
 def appendDictItems(req: dict, line: str, keys: list[str]) -> dict[str, list[str]]:
+    """
+    Tasks:
+    Since the line containing the recommended fix to the client already contains commas,
+    the line needs to be cleaned and appended in order to requirement dict
+    ---
+    Note:
+    The current format is:
+    Name of Setting (Human readable), name of setting (variable name), expected result, fix
+    ---
+    IN: the requirement dict
+    IN: the unformatted line (str)
+    IN: the list of keys as in the note above, so that each item is assigned to the correct dict item. (list[str])
+    OUT: the requirement dict with the new entry added
+    """
     itemsToAppend: list[str] = line.strip().split(",", maxsplit=3)
     for index, key in enumerate(keys):
         req[key].append(itemsToAppend[index])
@@ -55,6 +78,17 @@ def appendDictItems(req: dict, line: str, keys: list[str]) -> dict[str, list[str
 def parseClientTypeChart(
     clientTypeChart: Path, clientType: int
 ) -> dict[str, list[str]]:
+    """
+    Task:
+    Parses the client type chart to create a dict containing all the required configs
+    ---
+    Note:
+    the client type chart requires particular formatting, WIP autoformatter
+    ---
+    IN: path to client type chart csv file (Path)
+    IN: client type (int)
+    OUT: all requirements for given client type (dict)
+    """
     req: dict = {}
 
     # TODO this shit ugly, need to clean
@@ -72,24 +106,40 @@ def parseClientTypeChart(
 
 
 def checkPassFail(row: pd.DataFrame, req: dict[str, list[str]]) -> None:
+    """
+    Task:
+    Given an account id and the requirements for a given client type, print
+    pass/fail, and upon fail, print why and corrections to be done
+    ---
+    Note:
+    The statements use emojis, so some terminals may not have emoji support
+    ---
+    IN: a single row DataFrame containing the selected settings for a given acct id
+    IN: the list of required settings to validate against (dict)
+    OUT: None
+    """
     fixes: list[list[str]] = []
     incorrectSettings: list[list[str]] = []
 
-    # compare config vs expected values
+    # compare config vs expected values and add them to lists for printing
     for index, col in enumerate(req["Column Name"]):
         if row[col].lower() != req["Expected Response"][index]:
             fixes.append([req["Stripe Setting"][index], req["Guide for client"][index]])
             incorrectSettings.append([req["Expected Response"][index], str(row[col])])
 
-    # tell the user if fixes need to be made
+    # if fixes are empty
     if not fixes:
         print("Stripe configuration is set correctly! ✅")
+    # if fixes not empty
     else:
         print(
             f"Stripe configuration is not correct! ❌\nThere are {len(fixes)} fixes that need to addressed.\n"
         )
+        # print expected vs actual configs
         for i in incorrectSettings:
             print(f"Expected Response: {i[0]}\tActual Response: {i[1]}")
+        
+        # print fixes to copy paste to client
         print("Fixes to apply:")
         for i in fixes:
             print(
@@ -104,6 +154,20 @@ def configCheck(
     parseFile: Path,
     clientTypeChart: Path,
 ) -> None:
+    """
+    Task:
+    Takes an account id, the client type, Stripe export file and client type chart,
+    parses the given account id to find any missing or incorrect settings and prints them to stdout
+    ---
+    Notes:
+    WIP: account id string validation, more verbose client type declaration
+    ---
+    IN: Stripe account id (should look like acct_xxxxxxx) (str)
+    IN: client type as per the chart (int)
+    IN: path to most recent Stripe export file to parse (Path)
+    IN: path to client type chart containing the required settings (Path)
+    OUT: None
+    """
     if not parseFile.is_file():
         print(
             "Failed to open export.csv, please check that the file exists at the given path or the default path of ./export.csv"
